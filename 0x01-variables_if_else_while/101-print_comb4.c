879d62cd65b4d9f816e502551f31f27e35df2bd6
maxist101 <netochukwu26@gmail.com>
2023-08-11 18:45:24 -0700

    101print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ for (third_digit = second_digit + 1; third_digit <= 9; third_digit++)
 putchar(first_digit + '0');
 putchar(second_digit + '0');
 putchar(third_digit + '0');
-if (first_digit != 7  second_digit != 8  third_digit != 9)
+if (first_digit != 7 || second_digit != 8 || third_digit != 9)
 {
 putchar(',');
 putchar(' ');
